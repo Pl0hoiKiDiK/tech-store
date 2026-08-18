86bd9280af91fec54377048ea4ad4f77ccc3deac
Dora <itsmylifedasha@gmail.com>
2026-08-18 12:28:27 +0300

    chore: connect Inter font and global styles, add vercel config

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import './styles/variables.css';
 import './styles/global.css';
 
 const root = createRoot(document.getElementById('root'));
