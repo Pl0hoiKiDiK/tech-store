8158eb3e34823cc88f7fdb6a715ef5b48f29b461
Dora <itsmylifedasha@gmail.com>
2026-08-23 23:12:03 +0300

    feat(auth): restore session on reload and attach Bearer token to API requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import AppRoutes from './routes/AppRoutes';
+import { useGetMeQuery } from './features/auth/authApi';
+import { setCredentials, logout } from './features/auth/authSlice';
 
 export default function App() {
+  const dispatch = useDispatch();
+  const { accessToken, user } = useSelector((state) => state.auth);
+
+  const { data, error } = useGetMeQuery(undefined, {
+    skip: !accessToken || Boolean(user),
+  });
+
+  useEffect(() => {
+    if (data) {
+      dispatch(setCredentials({ user: data, accessToken }));
+    }
+    if (error) {
+      dispatch(logout());
+    }
+  }, [data, error, accessToken, dispatch]);
+
   return (
     <BrowserRouter>
       <AppRoutes />
--- src/app/api.js
+++ src/app/api.js
@@ -2,7 +2,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://dummyjson.com',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.accessToken;
+      if (token) headers.set('Authorization', `Bearer ${token}`);
+      return headers;
+    },
+  }),
   tagTypes: ['Products', 'Auth'],
   endpoints: () => ({}),
 });
